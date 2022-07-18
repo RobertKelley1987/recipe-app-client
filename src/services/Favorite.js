3365c82d33e6c90f377db3780c1734744af3f9f4
Robert Kelley <robertkelley1987@gmail.com>
2022-07-18 11:08:29 -0400

    Updated axios requests to backend to include withCredientials property

--- src/services/Favorite.js
+++ src/services/Favorite.js
@@ -1,14 +1,19 @@
 import axios from 'axios';
 import { SERVER_URL } from './server-url';
 
+const server = axios.create({
+    baseURL: SERVER_URL,
+    withCredentials: true
+});
+
 const Favorite = {
     // Get all favorites from app server
     getAll: async userId => {
-        const { data } = await axios.get(`${SERVER_URL}/users/${userId}/favorites`);
+        const { data } = await server.get(`/users/${userId}/favorites`);
         return data;
     },
     toggle: async (recipe, userId) => {
-        const { data } = await axios.post(`${SERVER_URL}/users/${userId}/favorites`, recipe);
+        const { data } = await server.post(`/users/${userId}/favorites`, recipe);
         return data;
     }
 }
--- src/services/User.js
+++ src/services/User.js
@@ -1,21 +1,26 @@
 import axios from 'axios';
 import { SERVER_URL } from './server-url';
 
+const server = axios.create({
+    baseURL: SERVER_URL,
+    withCredentials: true
+});
+
 const User = {
     logIn: async (email, password) => {
-        const { data } = await axios.post(`${SERVER_URL}/login`, { email: email, password: password });
+        const { data } = await server.post(`/login`, { email: email, password: password });
         return data;
     },
     logOut: async () => {
-        const { data } = await axios.post(`${SERVER_URL}/logout`);
+        const { data } = await server.post(`/logout`);
         return data;
     },
     signUp: async (email, password) => {
-        const { data } = await axios.post(`${SERVER_URL}/signup`, { email: email, password: password });
+        const { data } = await server.post(`/signup`, { email: email, password: password });
         return data;
     },
     validateSession: async () => {
-        const { data } = await axios.get(`${SERVER_URL}/sessions`);
+        const { data } = await server.get(`/sessions`);
         return data;
     }
 }
